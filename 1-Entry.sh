f50bd86a0233e924cb8f6c013a919efc437d42bb
Ayman Salah <ayman.1551.salah@gmail.com>
2023-01-13 13:19:28 +0200

    Entry File Done Partailly

--- 1-Entry.sh
+++ 1-Entry.sh
@@ -5,32 +5,25 @@ echo "Select An option From the Options Below !"
 select option in "->Press 1 To Create a Database" "->Press 2 To List available Databases" "->Press 3 To Connect to a Database" "->Press 4 To Drop a Database"
 # PS3="Please Choose a Number: "
 do
-case $option in
+case $REPLY in
 1) echo "First Option"
 # "->Press 1 To Create a Database") echo "First Option"
 # 1) echo "First Option"
-# NOTE: Both will Work 
-;;
+# NOTE: Both Above will Work But in Case Use $option and In select also Make it Option
+# [x] Or you Can use the $REPLY standard and Use Numbers 
+exit;;
 2) echo "Second Option"
 # "->Press 2 To List available Databases") echo "Second Option"
-;;
+exit;;
 3)  echo "Third Option"
 # "->Press 3 To Connect to a Database"
-;;
+exit;;
 4) echo "Fourth Option"
 # "->Press 4 To Drop a Database"
-;;
+exit;;
 *) echo "$REPLY is not Valid ! Only 1,2,3"
 ;;
+# NOTE: This is the Only One I made without Exit Since it will LOOP again ! 
+# In other Words it will Not Close the Script 
 esac
 done
-
-# case $REPLY in
-# 1)./createDB.sh
-# exit;;
-# 2) ./listDB.sh
-# exit;;
-# 3) ./connectDB.sh
-# exit;;
-# 4) ./dropDB.sh
-# exit;;
