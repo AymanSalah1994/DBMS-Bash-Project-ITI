89070b05275ab3903ae741246ce944f440a667e8
Ayman Salah <ayman.1551.salah@gmail.com>
2023-01-17 11:39:24 +0200

    C

--- 2.2.ListDatabases.sh
+++ 2.2.ListDatabases.sh
@@ -1,15 +1,17 @@
+#!/usr/bin/bash
+
 # NOTE: Since a Database = Directory ,,, Any Directory Means a DB ! 
 echo `clear`
 echo `ls -F  | grep "/" > dbnames` 
-echo `wc -l dbnames`
-echo \n
+echo `wc -l dbnames` 
+# echo `awk '{print($0)}' newLine` ; 
+echo `sed "p" newLine` 
+echo "*---------Databases with Their Tables-------------*" 
 echo "*---------Databases with Their Tables-------------*"
-echo \n
 for eachTable in `cat dbnames`
 do
-echo \n
+echo
 echo $eachTable 
 ls $eachTable
-echo "===End Of DataBase===="
-
+echo "====End Of DataBase===="
 done
