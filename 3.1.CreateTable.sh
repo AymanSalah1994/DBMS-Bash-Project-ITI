ed916934428a4705995fd61e2e8a2f99af920fbd
Ayman Salah <ayman.1551.salah@gmail.com>
2023-01-20 00:42:29 +0200

    Create Table DOne

---
+++ 3.1.CreateTable.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+
+function collectAllData
+{
+    typeset -i counter
+    typeset -i index
+    counter=1
+    index=0
+    while [ $index -ne $numberOfColumns ]
+    do
+        clear
+        echo "Enter The TYPE Of Column Nubmer ${counter}"
+        read -p "Enter 1 For String  , 2 for Integer ->$ " option
+        case $option in
+            1) echo "String"
+            ArrOfTypes[$index]="str"
+            ;;
+            2) echo "Integer"
+            ArrOfTypes[$index]="int"
+            ;;
+            *) echo "$option is not Valid ! Only 1,2 For STR Or INT "
+            continue
+            ;;
+        esac
+
+        read  -p  "Enter The NAME Of Column Nubmer ${counter} -$ " columnName
+                if [[ $columnName =~ ^[a-zA-Z0-9_]+$ ]] 
+                then 
+                    ArrOfColNames[$index]=$columnName
+
+                    # For Primary Key We Can Use Many Ways 
+                    if test -z $PK 
+                    # IF it is set , we will Not ask again
+                        then 
+                        echo "Is it the PRIMARY KEY?"
+                                read -p "Write 1 For YES ~$ " option
+                                case $option in
+                                    1) echo "String"
+                                    PK=$columnName
+                                    # Dollar Sign Means Value , without it 
+                                    # we Mean a Variable 
+                                    ;;
+                                esac
+                        fi
+                        # #####
+                else 
+                clear
+                echo "Not Valid Name" ;
+                echo "ERROR  , Try Creating the Table Again"
+                ./CreateTable.sh 
+                # exit 1 ; 
+                fi 
+        ((index = index + 1))
+        ((counter = counter + 1))
+    done
+}
+
+# Once It is Created , You Can insert and Update and Delete ...etc 
+PK=""
+read  -p "Enter the Name of Table ->" tableName
+if [[ $tableName =~ ^[a-zA-Z0-9_]+$ ]] 
+        then 
+            echo "Valid" ; 
+            typeset -i numberOfColumns
+            read  -p "Enter the Nubmer Of Columns For Table ->" numberOfColumns
+                if [[ $numberOfColumns -gt 0 ]]
+                then
+                echo "Valid Number"
+                # TODO enter the Loop For Creating the Structure
+                collectAllData 
+                else 
+                echo "Not a Valid Number , Try again , Enter Number greater than 0"
+                ./CreateTable.sh 
+                fi
+else 
+echo "Not Valid Name" ;
+./CreateTable.sh 
+fi 
+
+# echo ${ArrOfTypes[*]}
+# echo ${ArrOfColNames[*]}
+# echo "Primary"
+# echo "$PK"
+# TODO making a Check if PK is empty 
+
+
+
+
+
+if test -z $PK 
+    then 
+    echo "ERROR , you Did Not Specify the Primary Key !"
+    else 
+    cd $databaseName
+    touch "$tableName"
+    touch "+$tableName+types"
+    echo ${ArrOfTypes[*]} > "+$tableName+types"
+    touch "+$tableName+pk"
+    echo "$PK" > "+$tableName+pk"
+    touch "+$tableName+colNames"
+    echo ${ArrOfColNames[*]} > "+$tableName+colNames"
+fi
+
+
+# SUDO CODE FOR CREATE TABLE 
+
+# Create Table is All About Creation of the Struture Like any other DBMS 
+# Once It is Created , You Can insert and Update and Delete ...etc 
+
+# Ask For the Table Name 
+# you ask for the number of coulmns 
+# Make a loop for the Columns 
+# In each Loop ask If the PK is to Specify Or Not 
+# Three arrays to Fill three Files  [ pk , types , columns ]
+# If No PK , no Creation  , IF Any Interruption , RE-Call the File 
+# Create Table is For Creation OF Structure Only 
+# The go out to see What else to Do in the Main Menu [insert or whatever ]
--- 3.Connect.sh
+++ 3.Connect.sh
@@ -1,11 +1,38 @@
 #!/bin/bash
+# Connecting is ALL about "cd" to the Database Folder 
 
-echo $databaseName
-# CD To the DB folder 
-# Show the List 
-# Start Doing the List 
-# REMEBER : it ALL depend On the Creation Criteria We PUT !
+# Database Name is Exported 
+echo "=============================="
+echo "Connected To :$databaseName "
+echo "=============================="
+PS3="-~$ "
+select option in "->Press 1 To Create a Table" "->Press 2 To List Tables" "->Press 3 To Drop a Table" "->Press 4 To Insert to a Table"  "->Press 5 To Select from a Table" "->Press 6 To Delete From a Table" "->Press 7 To Update a Table"
+do
+case $REPLY in
+1) echo "Create Table"
+export databaseName
+./3.1.CreateTable.sh 
+exit;;
+2) echo "List Tables"
 cd $databaseName
-# ls 
-# pwd
-
+ls [a-zA-Z9-0_]*
+exit;;
+3)  echo "3 Option"
+./2.4.ConnectDatabase.sh
+exit;;
+4) echo "4 Option"
+./2.3.DropDatabase.sh
+exit;;
+5) echo "5 Option"
+./2.3.DropDatabase.sh
+exit;;
+6) echo "6 Option"
+./2.3.DropDatabase.sh
+exit;;
+7) echo "7 Option"
+./2.3.DropDatabase.sh
+exit;;
+*) echo "$REPLY is not Valid ! Only 1,2,3,4,5,6,7"
+;;
+esac
+done
