f485e221648a2bb874b7bf2c26cbb8cf7f1fa5a7
Ayman Salah <ayman.1551.salah@gmail.com>
2023-01-24 02:50:19 +0200

    Create DB OK

--- 2.1.CreateDB.sh
+++ 2.1.CreateDB.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
-
+clear
 read  -p "Enter the Name of the DataBase You want to Create ->" databaseName
 if [[ $databaseName =~ ^[a-zA-Z0-9_]+$ ]] 
-# We need the + Sign So Not an Empty string
         then 
-        echo "Valid" ; 
-        # Search for it And Either Create Or Reject 
         if [[ -d $databaseName ]] 
 			then
-		echo "The Database Exists , Please Choose another Name " ; 
-        else
-			mkdir $databaseName
-			echo "DataBase is Created" ;
-			notify-send  --icon=$PWD/check.png  $databaseName created
-			# $man notify-send 
-			# TODO connect to the New DB Directly 
+		notify-send  --icon=$PWD/cancel.png  "ERROR The Database Exists  , Please Choose another Name"
+		./2.1.CreateDB.sh
+			else
+				mkdir $databaseName
+				echo "DataBase is Created" ;
+				notify-send  --icon=$PWD/check.png  $databaseName created
+				export databaseName
+				./3.Connect.sh
 		fi
-else 
-echo "Not Valid Name , only Letters , numbers and Underscores" ;
-notify-send  --icon=$PWD/cancel.png  ERROR
+else
+echo "" ;
+notify-send  --icon=$PWD/cancel.png  "ERROR Not Valid Name , only Letters , numbers and Underscores"
+./2.1.CreateDB.sh
 fi 
 
