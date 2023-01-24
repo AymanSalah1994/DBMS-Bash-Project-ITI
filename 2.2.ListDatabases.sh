89bc25c5128b9f2fbe2670e0688c74b99512efd3
Ayman Salah <ayman.1551.salah@gmail.com>
2023-01-24 02:55:43 +0200

    List Databases OK

--- 2.2.ListDatabases.sh
+++ 2.2.ListDatabases.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/bash
 
-# NOTE: Since a Database = Directory ,,, Any Directory Means a DB ! 
-# We will Not make any other Directory for anything else inside the project folder
 clear
 ls -F  | grep "/" > DataBases
 wc -l DataBases
@@ -11,8 +9,6 @@ for eachDB in `cat DataBases`
 do
 echo "Database Name: "
 echo $eachDB 
-echo "Tables : "
-ls $eachDB
 echo "          ====End Of DataBase====         "
 done
-# TODO Enter Ok to contiune Or q To Quit
+./1.Entry.sh
